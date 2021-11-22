b612310549f6d4778471c7b202d501662bdbba9c
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2021-11-22 14:15:07 -0500

    popup_rpc/     got a version of async popup working.

--- popup_rpc/call_hmi_popup_client.py
+++ popup_rpc/call_hmi_popup_client.py
@@ -1,7 +1,35 @@
+import json
+import logging
 import rpyc
+import pickle
 
-if __name__ == '__main__':
-    server_ip = '127.0.0.1'
-    server_port = 1089710949
-    rpc_con = rpyc.connect(server_ip, server_port)
-    rpc_con.root.exposed_simplest_popup()
+
+# if __name__ == '__main__':
+server_ip = '127.0.0.1'
+server_port = 1089710949
+rpc_con = rpyc.connect(server_ip, server_port, config={'allow_public_attrs': True, 'sync_request_timeout': 10})
+# rpc_con.root.exposed_simplest_popup()
+
+
+lg = logging.getLogger('mds_popup_window')
+logging.basicConfig()
+lg.setLevel(logging.DEBUG)
+
+
+from main import get_dummy_dict
+
+msg_dict = get_dummy_dict(5.2)
+# # todo: getting an error about trying to send this dict, might just be much easier to send it as a json string and
+# #  wrap it on the other end...
+# rpc_con.root.exposed_show_popup_json(json.dumps(msg_dict))
+# rpc_con.root.exposed_show_popup_json(rpyc.async_(json.dumps(msg_dict)))
+
+# rpyc.core.netref.
+# rpyc.core.netref.builtins.dict()
+# nrd = rpyc.core.netref._normalized_builtin_types['builtins.dict']()
+# nrd.update(msg_dict)
+# rpc_con.root.exposed_show_popup(rpyc.async_(nrd))
+# rpc_con.root.exposed_show_popup_pickle(pickle.dumps(msg_dict))
+# rpc_con.root.exposed_show_popup_json_sync(msg_dict)
+if rpc_con.root.exposed_check_db_for_new_messages():
+    lg.debug('server successfully contacted.')
--- popup_rpc/hmi_server.py
+++ popup_rpc/hmi_server.py
@@ -1,9 +1,13 @@
 import datetime
+import json
 import logging
+import pickle
+import threading
 
 import rpyc
 from rpyc.utils.server import ThreadedServer
 
+from main import Popup
 
 lg = logging.getLogger('mds_popup_window')
 logging.basicConfig()
@@ -13,17 +17,54 @@ lg.setLevel(logging.DEBUG)
 date_time = datetime.datetime.now()
 
 
+def check_db_new_messages():
+
+    from main import get_dummy_dict
+
+    msg_dict = get_dummy_dict(5.2)
+
+    t1 = threading.Thread(target=Popup, args=(msg_dict,))
+    t1.start()
+    # Popup(json.loads(messages_json))
+    lg.debug('after')
+    return True
+
+
 class MonitorService(rpyc.Service):
     def on_connect(self, connection):
-        print('\nConnected on {}'.format(date_time))
+        lg.debug('\nConnected on {}'.format(date_time))
 
     def on_disconnect(self, connection):
-        print('Disconnected on {}\n'.format(date_time))
+        lg.debug('Disconnected on {}\n'.format(date_time))
 
-    def exposed_simplest_popup(self):
-        from main import dev_popup
+    # def exposed_simplest_popup(self):
+    #     from main import dev_popup
+    #
+    #     dev_popup()
+    #
+    # def exposed_show_popup(self, messages_dict):
+    #     rpyc.async_(lambda: Popup(messages_dict))
+    #
+    # def exposed_show_popup_json(self, messages_json):
+    #     rpyc.async_(lambda: Popup(json.loads(messages_json)))
+    #     Popup(json.loads(messages_json))
+    #
+    # def exposed_show_popup_pickle(self, messages_pickle):
+    #     rpyc.async_(lambda: Popup(pickle.loads(messages_pickle)))
+    #     Popup(json.loads(messages_pickle))
+    #
+    # def exposed_show_popup_json_sync(self, messages_json):
+    #     # TODO: try starting an async thread in a sync call, don't need rpyc's async, just some async
+    #     lg.debug('%s', messages_json)
+    #     t1 = threading.Thread(target=Popup, args=(messages_json,))
+    #     t1.start()
+    #     # Popup(json.loads(messages_json))
+    #     lg.debug('after')
+    #     return
 
-        dev_popup()
+    def exposed_check_db_for_new_messages(self):
+        """Start an asynchronous check for new messages in the database. If found, show popup message."""
+        check_db_new_messages()
 
 
 def start_server():
@@ -41,3 +82,4 @@ def start_server():
 
 if __name__ == "__main__":
     start_server()
+    # pass
