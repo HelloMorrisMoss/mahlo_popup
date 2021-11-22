b7fb8ef04563192f40219e910a5190be2f508bbf
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2021-11-22 16:45:34 -0500

    popup_rpc/hmi_server.py: moved the check check_db_new_messages() function into the MonitorService as a method. Added an __init__ method to the MonitorService class. Some attempts to make the window a singleton, not successful.

--- popup_rpc/hmi_server.py
+++ popup_rpc/hmi_server.py
@@ -17,25 +17,32 @@ lg.setLevel(logging.DEBUG)
 date_time = datetime.datetime.now()
 
 
-def check_db_new_messages():
-
-    from main import get_dummy_dict
-
-    msg_dict = get_dummy_dict(5.2)
-
-    t1 = threading.Thread(target=Popup, args=(msg_dict,))
-    t1.start()
-    # Popup(json.loads(messages_json))
-    lg.debug('after')
-    return True
-
-
 class MonitorService(rpyc.Service):
+    """Listens for 'Mahlo is stopped and there are new messages' signal to check the database, then displays a popup."""
+    def __init__(self):
+        super().__init__()
+        self.pop_up_showing = False
+        self.pop_up_thread = None
+
     def on_connect(self, connection):
         lg.debug('\nConnected on {}'.format(date_time))
 
     def on_disconnect(self, connection):
         lg.debug('Disconnected on {}\n'.format(date_time))
+
+    def check_db_new_messages(self):
+        from main import get_dummy_dict
+
+        msg_dict = get_dummy_dict(5.2)
+
+        if not self.pop_up_thread:
+            lg.debug('popup thread is %s', self.pop_up_thread)
+            self.pop_up_thread = threading.Thread(target=Popup, args=(msg_dict,), kwargs={'monitor_service': self})
+            self.pop_up_showing = True
+            self.pop_up_thread.start()
+            # Popup(json.loads(messages_json))
+            lg.debug('after')
+        return True
 
     # def exposed_simplest_popup(self):
     #     from main import dev_popup
@@ -54,7 +61,7 @@ class MonitorService(rpyc.Service):
     #     Popup(json.loads(messages_pickle))
     #
     # def exposed_show_popup_json_sync(self, messages_json):
-    #     # TODO: try starting an async thread in a sync call, don't need rpyc's async, just some async
+    #     # TO DO: try starting an async thread in a sync call, don't need rpyc's async, just some async;didn't work
     #     lg.debug('%s', messages_json)
     #     t1 = threading.Thread(target=Popup, args=(messages_json,))
     #     t1.start()
@@ -64,7 +71,7 @@ class MonitorService(rpyc.Service):
 
     def exposed_check_db_for_new_messages(self):
         """Start an asynchronous check for new messages in the database. If found, show popup message."""
-        check_db_new_messages()
+        self.check_db_new_messages()
 
 
 def start_server():
