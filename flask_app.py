5cc337282b537f781f592eadb261d5d2b6101c86
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2021-12-08 14:43:58 -0500

    flask_app.py: start_flask_app() now creates a background scheduled communications queue watcher. Created schedule_queue_watcher() function. main_app.py: now passing communication queues.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,9 @@
+from collections import deque
 from threading import Thread
 import flask
 from flask import g
 from flask_restful import Api
 import waitress
-
 
 from fresk.resources.signal_popup import Popup
 from fresk.sqla_instance import fsa
@@ -11,6 +11,8 @@ from fresk.sqla_instance import fsa
 from fresk.resources.defect import Defect, DefectList
 from db_uri import DATABASE_URI
 # from main import dev_popup
+
+from log_setup import lg
 
 app = flask.Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
@@ -22,6 +24,7 @@ app.debug = True
 
 api = Api(app)
 
+
 # def start_popup():
 #
 
@@ -32,6 +35,7 @@ def create_tables():
     # g.popup_thread = Thread(target=dev_popup)
     # g.popup = dev_popup()
 
+
 # @app.before_first_request
 # def create_popup():
 #     g.popup = dev_popup()
@@ -41,12 +45,44 @@ api.add_resource(Popup, '/popup')
 api.add_resource(DefectList, '/defects')
 
 
-def start_flask_app():
+def start_flask_app(in_message_queue=None, out_message_queue=None):
+    if in_message_queue is not None:
+        schedule_queue_watcher(in_message_queue, out_message_queue)
+    else:
+        lg.warning('No inbound message queue!')
+
     fsa.init_app(app)
     host = '0.0.0.0'
     port = 5000
     waitress.serve(app, host=host, port=port, threads=2)
-    # app.run(debug=True)
+
+
+def schedule_queue_watcher(in_message_queue, out_message_queue):
+    """Schedules a communications queue watcher to check the inbound queue for requests and send outbound messages.
+
+        :param in_message_queue: collections.deque
+        :param out_message_queue: collections.deque
+        """
+    import time
+    def regular_check_function():
+        # when it's ready, this will watch for requests from the popup (in_message_queue) and send the responses
+        # via the out_message_queue
+        import datetime
+        while len(in_message_queue):
+            print(f'message count: {len(in_message_queue)}')
+            print(in_message_queue)
+            try:
+                msg = in_message_queue.pop()
+                lg.debug(f'Regular check at {datetime.datetime.now()} found a message: {msg}')
+                out_message_queue.append(f'I got the message "{msg}"!')
+            except IndexError:
+                break  # the deque is empty TODO: the while and try except SHOULD be redundant
+            time.sleep(10)
+
+    from apscheduler.schedulers.background import BackgroundScheduler
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(regular_check_function, 'interval', seconds=30)
+    scheduler.start()
 
 
 if __name__ == '__main__':
--- main_app.py
+++ main_app.py
@@ -1,13 +1,29 @@
 import threading
 import tkinter
-import queue
+# import queue
+from collections import deque
 
 from flask_app import start_flask_app
 from main_window import dev_popup
 
-flask_thread = threading.Thread(target=start_flask_app)
+
+# allow communication
+f2p_queue = deque()
+p2f_queue = deque()
+
+flask_thread = threading.Thread(target=start_flask_app, args=(p2f_queue, f2p_queue))
 flask_thread.setDaemon(True)
 flask_thread.start()
+
+import  datetime
+import time
+
+while True:
+    time.sleep(20)
+    p2f_queue.append(f'popup sent msg at {datetime.datetime.now()}')
+    while f2p_queue:
+        print(f'message back: {f2p_queue.pop()} at {datetime.datetime.now()}')
+
 
 # root = tkinter.Tk()
 
