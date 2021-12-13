c995091a9cbae9562c28442e438a0cbdc24a439d
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2021-12-13 17:54:13 -0500

    publishing_vars.py: more cleanup and documentation. The PublishingLengthList class now supports iadd (+=), and item deletion. There is an option to allow slices to publish using the same subscribers. If reassigning the variable holding the original to a slice of the same list this could be desirable; however, a slice as another concurrent instance could cause confusion as they both would report their own lengths. So, the option is default to being disabled.

--- publishing_vars.py
+++ publishing_vars.py
@@ -2,6 +2,13 @@ import tkinter
 
 
 def upsert_subscriber(self, subscriber, update_method):
+    """Add or update a subscriber.
+
+    :param self: type, the class instance whose subscriber list will be updated.
+    :param subscriber: any, the subscriber to add or update.
+    :param update_method: callable, the method or function to call using when publishing the new value.
+    :return: str, 'subscriber updated' or 'subscriber added'
+    """
     for sub_num, subr in enumerate(self._length_subscribers):
         if subr['subscriber'] is subscriber:
             self._length_subscribers[sub_num] = {'subscriber': subscriber, 'update_method': update_method}
@@ -56,67 +63,140 @@ def publishing_var(var_type, *fn_args, **fn_kwargs):
     return PublishingVar(*fn_args, **fn_kwargs)
 
 
-class MetaWrap(type):
+def create_wrapper_for_class_methods(the_wrapped_class):
+    """Get a method wrapper factory function for the class.
+
+    :param the_wrapped_class: type, the class to get a method wrapper factory function for.
+    :return: callable, the method wrapping factory function.
+    """
+
+    # the method wrapper factory function
+    def method_publish_wrapper(method):
+        """get the method wrapped in the update function."""
+
+        # the method wrapper function
+        def update_function(*args, **kwargs):
+            """Update any subscribers the new length after performing the original method, then return its return.
+
+            :param args: positional arguments to pass to the original method.
+            :param kwargs: keyword arguments to pass to the original method.
+            :return: any, the return value of the original method.
+            """
+
+            return_val = method(*args, **kwargs)  # the value returned be the original method
+            self = args[0]  # calling this out of the class scope, self isn't automatically handed to the publish method
+            the_wrapped_class.publish(self)  # so we borrow it from the method call that was in the class scope
+            return return_val
+        return update_function
+    return method_publish_wrapper
+
+
+class PublishListMetaWrap(type):
     """A metaclass that wraps the generated class' methods with a publish wrapper function."""
 
     def __new__(cls, name, base_classes, dct):
         wrapped_class = super().__new__(cls, name, base_classes, dct)
-        wrap_methods = 'append', 'insert', 'pop', 'remove', 'extend', 'clear'#, '__add__'
+        wrap_methods = ['append', 'insert', 'pop', 'remove', 'extend', 'clear', '__delitem__']
 
-        def __add__(self, other):
-            return PublishingLengthList(list.__add__(self, other))
+        # add needs to return a publishing list or the publishing functionality would be lost, for: +=
+        def __iadd__(self, other):
+            return_value = PublishingLengthList(list.__add__(self, other))
+            return_value._length_subscribers = self._length_subscribers
+            return_value.publish()
+            return return_value
 
+        setattr(wrapped_class, '__iadd__', __iadd__)  # replace with the wrapped method
 
-        # some specific additions
-        setattr(wrapped_class, '_length_subscribers', [])
-        # setattr(wrapped_class, '__add__', )
+        # get a wrapping factory function for this class' methods
+        wrap_publish = create_wrapper_for_class_methods(wrapped_class)
 
-        # the wrapper function
-        def wrap_publish(method):
-            """get the method wrapped in the update function."""
-            def update_function(*args, **kwargs):
-                """Update any subscribers the new length after performing the original method, then return its return.
-
-                :param args: positional arguments to pass to the original method.
-                :param kwargs: keyword arguments to pass to the original method.
-                :return: any, the return value of the original method.
-                """
-                return_val = method(*args, **kwargs)
-                # print(f'{return_val=} - {args=}, {kwargs=}, {wrapped_class=}')
-                for subr in wrapped_class._length_subscribers:
-                    wc_self = args[0]
-                    subr['update_method'](wc_self.__len__())
-                return return_val
-            return update_function
-
-        # this is only intended for one base class (list), this would check all base classes, could be good or bad
+        # this is only intended for one base class (list), this would iterate all base classes, could be good or bad
         for base_class in base_classes:
-            for field_name, field in base_class.__dict__.items():
-                if callable(field):
-                    if field_name in wrap_methods:
-                        setattr(wrapped_class, field_name, wrap_publish(field))  # replace with the wrapped method
+            for attribute_name, attribute in base_class.__dict__.items():
+                if callable(attribute):
+                    if attribute_name in wrap_methods:
+                        setattr(wrapped_class, attribute_name,
+                                wrap_publish(attribute))  # replace with the wrapped method
+
+        wrap_methods += ['__iadd__',]
+        setattr(wrapped_class, '_publishing_methods', wrap_methods)  # to make this accessible if needed
         return wrapped_class
 
 
-class PublishingLengthList(list, metaclass=MetaWrap):
-    """A list with the subscriber pattern for the length.
+class PublishingLengthList(list, metaclass=PublishListMetaWrap):
+    """A list with the subscriber/publisher design pattern for the length.
 
     If a method is called that may change the list length any subscribers will have their methods run with the new
-    length used as the parameter.
+    length used as the parameter. There are some important design choices regarding what will retain publishing and
+    what will not. The goal being to prevent unintentionally creating multiple competing publishers.
+
+    The consuming method should take the length, an int, and be prepared to be updated when the value isn't different.
+    An example of why this could happen is adding an empty list (see below).
+
+    ex:
+        > items = 1, 2, 3
+        > plist = PublishingLengthList(items)
+        > plist._publishing_methods  # this will return the names of the methods that will publish to subscribers
+        'append', 'insert', 'pop', 'remove', 'extend', 'clear', '__delitem__'  # * see slices below *
+
+        # add a subscriber
+        > plist.subscribe('any subscriber object, lambda x: print(f'updated to length: {x}'))
+        > plist.publish()  # this can be used to publish the initial state, or at any time to update subscribers
+        updated to length: 3
+
+        # plist methods - below are examples of a few of the publishing methods using the subscriber above
+        > plist.append(4)  # the publishing methods will call the update_method with the length as a parameter
+        updated to length: 4
+        > b_list = plist + [6, 7, 8]  # b_list will not update, it is a normal list * PUBLISHING LOST *
+        > plist +=  [6, 7, 8]  # iadd will retain publishing and update the subscribers
+        updated to length: 7
+        > plist += []  # adding an empty list will update subscribers with the same length as before
+        updated to length: 7
+        > plist.pop(2)
+        updated to length: 6
+
+        # slices
+        > s_list = plist[1:3]  # by default slicing will not retain publishing (normal list) * PUBLISHING LOST *
+        > s_list.append(12)
+        > plist.slices_publish = True  # slices retaining publishing can be enabled for uses such sd this:
+        > plist = plist[1:3]
+        updated to length: 2
+        > plist._publishing_methods  # with slices_publish enabled, 'slice' will be added to _publishing_methods
+        ['append', 'insert', 'pop', 'remove', 'extend', 'clear', '__delitem__', '__iadd__', 'slice']
+        # note: the method overridden is actually __getitem__, ._publishing_methods was intended for ease of reference
 
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._length_subscribers = []
+        self._slices_publish = False  # whether slices should publish their size (default False)
+
+    @property
+    def slices_publish(self):
+        """Whether slices should inherit publishing functionality and subscribers list, True/False (default: False)"""
+        return self._slices_publish
+
+    @slices_publish.setter
+    def slices_publish(self, new_value):
+        self._slices_publish = new_value
+        try:
+            if new_value:
+                self._publishing_methods.append('slice')
+            else:
+                self._publishing_methods.pop(self._publishing_methods.index('slice'))
+        except AttributeError:
+            pass  # this is just informational, if it's missing, not much of a problem
 
     def subscribe(self, subscriber, update_method):
-        """Subscribe an object to the list length.
+        """Subscribe to the list length, receiving updates when it changes.
 
         Note:
         The update_method doesn't have to have anything to do with the subscriber, though it is a good idea. The
         subscriber is used to prevent multiple subscription and allow for changing the update_method.
 
         :param subscriber: object
-        :param update_method: method, method or function to be called and passed the new value when the string is set.
+        :param update_method: method, method or function to be called and passed the new length when it changes.
         :return: str, either 'subscriber added' or 'subscriber updated' if the subscriber was already subscribed.
         """
         try:
@@ -125,18 +205,48 @@ class PublishingLengthList(list, metaclass=MetaWrap):
             self._length_subscribers = []
         return upsert_subscriber(self, subscriber, update_method)
 
-    def __add__(self, other):
-        return PublishingLengthList(list.__add__(self, other))
+    def unsubscribe(self, subscriber):
+        """Remove a subscriber from the subscriber list.
+
+        :param subscriber: any, the subscriber to remove.
+        :return: bool, if the subscriber was found will return True, if they were not found False
+        """
+        for sub_num, subr in enumerate(self._length_subscribers):
+            if subr == subscriber:
+                del self._length_subscribers[sub_num]
+                return True
+        return False
+
+    def publish(self):
+        """Update the subscribers with what the length is now."""
+        for subr in self._length_subscribers:
+            subr['update_method'](self.__len__())
+
+    def __getitem__(self, item):
+        """If self.slices_publish is set to True, then slices returned will publish their length as the original.
+
+            If self.slices_publish is False or the item is not a slice, behaves as list.__getitem__.
+        :param item: any, the item to get (such as a slice) or self.__dict__ key.
+        :return: any
+        """
+        gotitem = super().__getitem__(item)
+        if isinstance(item, slice) and self.slices_publish:
+            new_plist = PublishingLengthList(gotitem)
+            new_plist._length_subscribers = self._length_subscribers
+            new_plist.publish()
+            return new_plist
+        else:
+            return gotitem
 
 
 if __name__ == '__main__':
+    # some rough testing
     a_list = PublishingLengthList((1, 2, 3))
-    a_list.subscribe(27, lambda x: print(f'updated to length: {x}'))
-    print(a_list)
+    a_list.subscribe('any subscriber object', lambda x: print(f'updated to length: {x}'))
+    print(f'check the list {a_list}')
     a_list.append(4)
     a_list.append(5)
-    print(a_list)
+    print(f'check the list {a_list}')
     print('The list appended correctly', a_list == [1, 2, 3, 4, 5])
     b_list = a_list + [6, 7, 8]
     print(b_list)
-
