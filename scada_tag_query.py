ce761edf7042ca676726ac73ba81ac32339e92b0
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2022-02-08 10:17:15 -0500

    Further abstract the tag history query functionality
    More than recent lots will be queried in the future. The query for .get_recent_lots() has been generalized and moved to
    .get_recent_values(). TagIds class to organize tag entry information.

--- scada_tag_query.py
+++ scada_tag_query.py
@@ -6,6 +6,36 @@ from log_setup import lg
 from untracked_config.th_creds import connection_dict
 
 
+class TagIds:
+    """A class to hold tag ids."""
+
+    def __init__(self, tag_set_tids):
+        self._tag_set = tag_set_tids
+        if tag_set_tids is not None:
+            self._tag_set_dict = {
+                'lam1': {
+                    'lot_number': 'mahlo/lam1/batchid',
+                    'length': 'MAHLO/LAM1/MdMeterCount',
+                    },
+                'lam2': {
+                    'lot_number': 'mahlo/lam2/batchid',
+                    'length': 'MAHLO/LAM2/MdMeterCount',
+                    },
+                }
+            # for development
+            self._tag_set_dict['lam0'] = self._tag_set_dict['lam1']
+
+    def process_paths(self, paths_to_id_function):
+        """Process the tag ilike string to a tag id using the provided function.
+
+        :param paths_to_id_function: callable, a function that takes an ilike pattern string and returns a tag id int.
+        """
+
+        for field, ilike_str in self._tag_set_dict[self._tag_set].items():
+            lg.debug('setting %s field using %s', field, ilike_str)
+            setattr(self, field, paths_to_id_function(ilike_str))
+
+
 class DatabaseConnection:
     """A database connection template."""
 
@@ -14,6 +44,7 @@ class DatabaseConnection:
 
         :param connection_dictionary: dict, required parameters to pass to psycopg2.connect().
         """
+
         connection_dictionary = connection_dict if (connection_dictionary is None) else connection_dictionary
         self.cnn = connect(*args, **connection_dictionary, **kwargs)
         self.cnn.autocommit = True
@@ -21,24 +52,23 @@ class DatabaseConnection:
 
 
 class TagHistoryConnector(DatabaseConnection):
-    """A database connection for querying Ignition tag history from a postgres/Timescale database for a 'machine'."""
+    """A database connection for querying Ignition tag history from a postgres/Timescale database for a 'machine'.
+
+    Tag ids are updated when instantiated. If a tag is removed and recreated or renamed (but still matches the ilike
+    string) then it will query the new tag after reinstantiation or calling .update_tag_ids().
+    """
 
     def __init__(self, tag_set=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._history_table = sql.Identifier('sqlth_1_data')
         self._tag_entry_table = sql.Identifier('sqlt_te')
+        self.tag_ids = TagIds(tag_set_tids=tag_set)
+        self.update_tag_ids()
 
-        self.tag_ids = {}
+    def update_tag_ids(self):
+        """Query the tag entry table for the tag ids matching the preset ilike strings."""
 
-        if tag_set is not None:
-            tag_set_dict = {
-                'lam1': {'lot_number': 'mahlo/lam1/batchid'},
-                'lam2': {'lot_number': 'mahlo/lam2/batchid'},
-                }
-            tag_set_dict['lam0'] = tag_set_dict['lam1']
-            for field, ilike_str in tag_set_dict[tag_set].items():
-                lg.debug('setting %s field using %s', field, ilike_str)
-                self.tag_ids[field] = self.tag_id_query(ilike_str)
+        self.tag_ids.process_paths(self.tag_id_query)
 
     def tag_id_query(self, tag_path_ilike_string):
         """Get the tag id that matches the tag path string.
@@ -54,16 +84,26 @@ class TagHistoryConnector(DatabaseConnection):
             raise Warning('Multiple tags match ilike string pattern (tag_path_ilike_string): %s', tag_path_ilike_string)
         return results[0]
 
-    def get_recent_lots(self, lot_count=5):
+    def get_recent_lots(self, lot_count: int = 5):
         """Get lot_count most recent lot numbers in increasing age order.
 
         :param lot_count: int, the number of lot numbers to get.
         :return: list, of up to lot_count tuples, each containing only the lot number (string).
         """
 
-        lot_query = sql.SQL(r'''SELECT stringvalue FROM sqlth_1_data WHERE tagid = %s ORDER BY t_stamp 
-        DESC LIMIT %s;''')
-        self.crs.execute(lot_query, (self.tag_ids['lot_number'], lot_count))
+        results = self.get_recent_values(self.tag_ids.lot_number, lot_count)
+        return [row[3] for row in results]
+
+    def get_recent_values(self, tag_id, value_count=5):
+        """Get value_count most recent values in increasing age order (most recent first).
+
+        :param tag_id: int, the tag id in the tag history database.
+        :param value_count: int, the number of lot numbers to get.
+        :return: list, of 0 to lot_count tuples, each containing a row from the tag history.
+        """
+
+        lot_query = sql.SQL(r'''SELECT * FROM sqlth_1_data WHERE tagid = %s ORDER BY t_stamp DESC LIMIT %s;''')
+        self.crs.execute(lot_query, (tag_id, value_count))
         return self.crs.fetchall()
 
     def current_lot_number(self):
