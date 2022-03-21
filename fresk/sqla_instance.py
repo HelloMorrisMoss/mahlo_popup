54bb70eafbc1657d5a97a89d68b3b4794967fbff
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2022-03-21 17:05:49 -0400

    Fix save defect requiring multiple clicks due to connection idle

--- fresk/sqla_instance.py
+++ fresk/sqla_instance.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.scoping import scoped_session
 from untracked_config.db_uri import DATABASE_URI
 
 # engine = create_engine(DATABASE_URI, connect_args={'check_same_thread': False})
-engine = create_engine(DATABASE_URI)  # , connect_args={'check_same_thread': False})
+engine = create_engine(DATABASE_URI, pool_pre_ping=True)
 local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 Session = scoped_session(local_session)
