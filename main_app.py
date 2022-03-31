7f8fc561f402e2d5a7313a9860912cf1a74dc4d2
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2022-03-31 16:36:04 -0400

    Don't create popup when not on laminator HMI

--- main_app.py
+++ main_app.py
@@ -4,16 +4,23 @@ import threading
 from collections import deque
 
 from fresk.flask_app import start_flask_app
+from log_setup import lg
 from main_window import MainWindow
 
 # allow communication
 f2p_queue = deque()
 p2f_queue = deque()
 
-# start the flask app in its own thread
-flask_thread = threading.Thread(target=start_flask_app, args=(p2f_queue, f2p_queue))
-flask_thread.setDaemon(True)
-flask_thread.start()
+from untracked_config.lam_num import LAM_NUM
 
-# start the popup (tkinter requires the main thread)
-MainWindow(inbound_queue=f2p_queue, outbound_queue=p2f_queue)
+if LAM_NUM:
+    # start the flask app in its own thread
+    flask_thread = threading.Thread(target=start_flask_app, args=(p2f_queue, f2p_queue))
+    flask_thread.setDaemon(True)
+    flask_thread.start()
+
+    # start the popup (tkinter requires the main thread)
+    MainWindow(inbound_queue=f2p_queue, outbound_queue=p2f_queue)
+else:
+    lg.info('Running webserver without popup.')
+    start_flask_app(p2f_queue, f2p_queue)
