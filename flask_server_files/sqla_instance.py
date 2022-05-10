ef137d1d797c8b312b40923b482128ed0fde7d20
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2022-05-10 17:39:09 -0400

    Test for SQLAlchemy 2.0 migration issues (none found)

--- flask_server_files/sqla_instance.py
+++ flask_server_files/sqla_instance.py
@@ -1,3 +1,5 @@
+import os
+
 import flask_sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -5,10 +7,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.scoping import scoped_session
 
 from untracked_config.db_uri import DATABASE_URI
+from untracked_config.development_node import ON_DEV_NODE
+
+os.environ['SQLALCHEMY_WARN_20'] = "true"
 
 # engine = create_engine(DATABASE_URI, connect_args={'check_same_thread': False})
-engine = create_engine(DATABASE_URI, pool_pre_ping=True)
-local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+echo = True if ON_DEV_NODE else False
+engine = create_engine(DATABASE_URI, pool_pre_ping=True, echo=echo, future=True)
+local_session = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 Base = declarative_base()
 Session = scoped_session(local_session)
 Base.session = Session
