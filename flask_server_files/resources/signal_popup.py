5cf00b16145304cf1054b61ab25d6f28147d5126
HelloMorrisMoss <56200569+HelloMorrisMoss@users.noreply.github.com>
2022-10-03 16:52:49 -0400

    Add a web-interface for supervisors to troubleshoot the popup.

--- flask_server_files/resources/signal_popup.py
+++ flask_server_files/resources/signal_popup.py
@@ -3,30 +3,54 @@ from flask_restful import reqparse, Resource
 from flask_server_files.queuesholder import queues
 from log_setup import lg
 
+action_dict = {
+    'shrink':
+        {'debug_message': 'shrinking popup',
+         'action_params': {'action': 'shrink'},
+         'return_result': ({'popup_result': 'Shrinking popup window.'}, 200),
+         'description': 'Shrink the window to button size, no change if already button.',
+         },
+    'show':
+        {'debug_message': 'showing popup',
+         'action_params': {'action': 'show'},
+         'return_result': ({'popup_result': 'Showing popup window.'}, 200),
+         'description': 'Show the full window if there are defects active, no change if already full.',
+         },
+    'show_force': {
+        'debug_message': 'showing popup',
+        'action_params': {'action': 'show_force'},
+        'return_result': ({'popup_result': 'Showing popup window (force).'}, 200),
+        'description': 'Show the full window even if no defects active, no change if already full.',
+        },
+    'defects_updated': {
+        'debug_message': 'tell popup there are defect updates',
+        'action_params': {'action': 'check_defect_updates'},
+        'return_result': ({'popup_result': 'Checking for new defects.'}, 200),
+        'description': 'Tell the window to check for updates to the defects in the database.',
+        },
+    'reset_position':
+        {'debug_message': 'tell popup to return to screen origin',
+         'action_params': {'action': 'reset_position'},
+         'return_result': ({'popup_result': 'Resetting popup position.'}, 200),
+         'description': 'Return the window to the top left corner of the screen.',
+         },
+    'restart_popup':
+        {'debug_message': 'tell popup to restart',
+         'action_params': {'action': 'restart_popup'},
+         'return_result': ({'popup_result': 'Restarting popup.'}, 200),
+         'description': 'Close the popup program entirely (including the web server) and start it again.',
+         },
+    'shift_change': {
+        'debug_message': 'tell popup the shift',
+        'action_params': {'action': 'shift_change'},
+        'return_result': ({'popup_result': 'Changing shift'}, 200),
+        'description': 'Tell the popup that a shift change is happening, resetting the operator selected to "NO '
+                       'OPERATOR".',
+        }}
+
 
 class Popup(Resource):
-    # def __init__(self):
-    #     pass
-        # if getattr(g, 'popup') is None:
-        #     self.get()
-        # self.popup = g.popup
 
-    # def show(self):
-    #     import threading
-    #
-    #     # g.popup_thread = threading.Thread(target=dev_popup)
-    #
-    #
-
-    #     # the laminator is stopped, show the full popup
-    #     # do we want to have this happen automatically? maybe not?
-    #     # self.popup
-    #     pass
-    #
-    # def hide(self):
-    #     # the laminator is moving
-    #     pass
-    #
     def get(self):
         # g.popup = dev_popup(json.dumps(get_dummy_dict(5.6)))
         raise NotImplementedError('This had been built with dicts and has not been remade using sqlalchemy version.')
@@ -34,43 +58,18 @@ class Popup(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('action', type=str, required=True, help='You must provide a command action.')
+        # parser.add_argument('source', type=str, required=True, help='You must provide a command action.')
 
         data = parser.parse_args()
         lg.debug('action received: %s', data['action'])
 
-        if data['action'] == 'shrink':
-            queues.out_message_queue.append({'action': 'shrink'})
-            return {'popup_result': 'Shrinking popup window.'}, 200
-
-        elif data['action'] == 'show':
-            lg.debug('showing popup')
-            queues.out_message_queue.append({'action': 'show'})
-            return {'popup_result': 'Showing popup window.'}, 200
-
-        elif data['action'] == 'show_force':
-            lg.debug('showing popup')
-            queues.out_message_queue.append({'action': 'show_force'})
-            return {'popup_result': 'Showing popup window (force).'}, 200
-
-        elif data['action'] == 'defects_updated':
-            lg.debug('tell popup there are defect updates')
-            queues.out_message_queue.append({'action': 'check_defect_updates'})
-            return {'popup_result': 'Checking for new defects.'}, 200
-
-        elif data['action'] == 'reset_position':
-            lg.debug('tell popup to return to screen origin')
-            queues.out_message_queue.append({'action': 'reset_position'})
-            return {'popup_result': 'Resetting popup position.'}, 200
-
-        elif data['action'] == 'restart_popup':
-            lg.debug('tell popup to restart')
-            queues.out_message_queue.append({'action': 'restart_popup'})
-            return {'popup_result': 'Restarting popup.'}, 200
-
-        elif data['action'] == 'shift_change':
-            lg.debug('tell popup the shift')
-            queues.out_message_queue.append({'action': 'shift_change'})
-            return {'popup_result': 'Changing shift'}, 200
-
+        action_to_take = action_dict.get(data['action'])
+        if action_to_take is not None:
+            lg.debug('Action to take: %s', action_to_take)
+            if db_msg := action_to_take.get('debug_message') is not None:
+                lg.debug(db_msg)
+            queues.out_message_queue.append(action_to_take['action_params'])
+            return action_to_take['return_result']
         else:
+            lg.debug('Did not receive a valid action.')
             return {'popup_result': 'No valid request.'}, 400
--- flask_server_files/routing.py
+++ flask_server_files/routing.py
@@ -6,48 +6,76 @@ defects_table:  /defect_table renders a simple html view of the defects in the d
 import datetime
 
 import flask
+import requests
+from flask import request
 
 from flask_server_files.resources.defect import DefectList
+from flask_server_files.resources.signal_popup import action_dict
 from log_setup import lg
+from untracked_config.lam_num import LAM_NUM
 
 routes_blueprint = flask.Blueprint('routes', __name__, template_folder='flask_server_files.templates')
 
 
 @routes_blueprint.route('/defect_table')
 def defects_table():
-	"""Return an HTML/website for viewing defects.
+    """Return an HTML/website for viewing defects.
 
 	:return:
 	"""
-	import pandas as pd
-	return pd.DataFrame.from_dict(DefectList().get()[0]).T.to_html()
+    import pandas as pd
+    return pd.DataFrame.from_dict(DefectList().get()[0]).T.to_html()
 
 
 @routes_blueprint.route('/popup_status')
 def operational_check():
-	"""Ask the popup if it is operational."""
-	from flask import current_app as app
+    """Ask the popup if it is operational."""
+    from flask import current_app as app
 
-	# send a request for a popup status report
-	app.out_message_queue.append({'action': 'popup_status_check'})
-	start_time = datetime.datetime.now()
-	mn = 0
-	status_received = None
+    # send a request for a popup status report
+    app.out_message_queue.append({'action': 'popup_status_check'})
+    start_time = datetime.datetime.now()
+    mn = 0
+    status_received = None
 
-	# wait 5 seconds, while checking for a status message
-	while (start_time - datetime.datetime.now()).total_seconds() < 5:
-		for mn, msg in enumerate(app.in_message_queue):
-			status_received = msg.get('popup_status')
+    # wait 5 seconds, while checking for a status message
+    while (start_time - datetime.datetime.now()).total_seconds() < 5:
+        for mn, msg in enumerate(app.in_message_queue):
+            status_received = msg.get('popup_status')
 
-			# ocne a message has been received stop checking
-			if status_received:
-				break
-		if status_received:
-			break
+            # ocne a message has been received stop checking
+            if status_received:
+                break
+        if status_received:
+            break
 
-	# pop the message from the deque
-	p_status = app.in_message_queue[mn]
-	del app.in_message_queue[mn]
-	lg.debug(p_status)
+    # pop the message from the deque
+    p_status = app.in_message_queue[mn]
+    del app.in_message_queue[mn]
+    lg.debug(p_status)
 
-	return p_status, 200
+    return p_status, 200
+
+
+act_keys = action_dict.keys()
+
+
+@routes_blueprint.route('/controls', methods=['GET', 'POST'])
+def supervisory_controls_page():
+    """A web page with troubleshooting controls for the defect popup.
+
+    :return: str, html
+    """
+    form_response = None
+    if request.method == 'GET':
+        lg.debug('supervisory page loading.')
+    elif request.method == 'POST':  # button pressed
+        lg.debug('supervisory page POST RECIEVED')
+        for actn in act_keys:  # loop through the button actions and send a request to this web server for the action
+            lg.debug('Checking for %s', actn)
+            if action_requested := request.form.get(actn):
+                lg.debug('Action requested: %s', action_requested)
+                form_response = requests.post('http://localhost:5000/popup', data={'action': action_requested})
+
+    return flask.render_template('pop_up_supervisory_controls.html', action_list=act_keys, action_dict=action_dict,
+                                 lam_num=LAM_NUM, form_response=form_response)
